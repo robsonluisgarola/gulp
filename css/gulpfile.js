18b5648d0068a4e4adbaf41c670b81306824b63f
Robson Garola <robsongarola@hotmail.com>
2021-08-08 10:04:51 -0300

    confirmo alteracoes

---
+++ css/gulpfile.js
@@ -0,0 +1,16 @@
+const gulp= require('gulp');
+const {series}=require('gulp');
+const uglifycss=require('gulp-uglifycss');
+const sass = require('gulp-sass');
+const concat= require('gulp-concat');
+
+function transformacaoCss(){
+
+    return gulp.src('src/sass/index.scss')
+            .pipe(sass().on('error',sass.logError))
+            .pipe(uglifycss({'uglyComments':true}))
+            .pipe(concat('estilo.min.css'))
+            .pipe(gulp.dest('build/css'))
+}
+
+module.exports.default= series(transformacaoCss)
