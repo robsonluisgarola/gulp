412032d5f137e385f7bc4f5fe57e5b8fef08b934
Robson Garola <robsongarola@hotmail.com>
2021-08-09 17:35:28 -0300

    atualiza09_08_21_001

--- css/gulpfile.js
+++ css/gulpfile.js
@@ -1,8 +1,9 @@
 const gulp= require('gulp');
-const {series}=require('gulp');
+const {series,parallel}=require('gulp');
 const uglifycss=require('gulp-uglifycss');
 var sass = require('gulp-sass')(require('sass')); // a linha correta é está. o gulp-sass 5 não tem copilador.
 const concat= require('gulp-concat');
+ 
 
 function transformacaoCss(){
 
@@ -13,4 +14,13 @@ function transformacaoCss(){
             .pipe(gulp.dest('build/css'))
 }
 
-module.exports.default= series(transformacaoCss)
+
+function transportaHTML(){
+
+    return gulp.src('src/index.html')
+                  .pipe(concat('index.min.html'))
+             //   .pipe(htmlmin({collapseWhitespace:true}))
+                .pipe(gulp.dest('build'))
+}
+
+module.exports.default= parallel(transformacaoCss,transportaHTML)
